f8620107ecd5ed3f9fcd65aef11f2b914b2347b3
Javier González Giménez <javiergonzalezgimenez@Javiers-MacBook-Air.local>
2024-06-08 12:05:59 +0200

    first commit

---
+++ CursoiOS.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+var greeting = "Hello, playground"
+var name = "javi"
+let surname = "gonzalez"
+name = "javier"
+
+// TIPOS DE VARIABLES
+
+//CHAR
+var symbol:Character = "e"
+
+//STRING
+var dogName:String = "Bimbo"
+
+//INT
+var year:Int = 2024
+
+//FLOAT
+var yearF:Float = 2024.2
+
+//DOUBLE
+var yearD:Double = 2024.333333333333333
+
+//BOOL
+var imHappy:Bool = true
+
+
+
+
